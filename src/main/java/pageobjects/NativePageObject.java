c03cfb621b3ed6ff1b01c0d16d0ae5cbec5f6ab6
iRezograf <rafael_reziapov@epam.com>
2021-06-18 14:51:45 +0300

    Have problem with  @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout" +                               "/android.widget.FrameLayout/android.view.ViewGroup" +                               "/android.widget.FrameLayout[2]/android.widget.LinearLayout" +                               "/android.widget.ScrollView" +                               "/android.widget.LinearLayout/android.widget.CheckedTextView")                       WebElement CheckedTextView;

--- src/main/java/pageobjects/NativePageObject.java
+++ src/main/java/pageobjects/NativePageObject.java
@@ -2,14 +2,65 @@ package pageobjects;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AndroidFindBys;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class NativePageObject  {
 
+    //  Sign In
+    @AndroidFindBy(id ="platkovsky.alexey.epamtestapp:id/login_email")
+    WebElement loginEmailField;
+
+    @AndroidFindBy(id ="platkovsky.alexey.epamtestapp:id/login_pwd")
+    WebElement login_pwdField;
+
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInBtn;
+
+
+    // Create New Account
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement registerBtn;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
+    WebElement eMailField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
+    WebElement usernameField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
+    WebElement passwordField;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
+    WebElement passwordConfirmField;
+
+    //agreement
+    @AndroidFindBy(id = "android.widget.CheckedTextView")
+    WebElement CheckTextView;
+
+    //@FindBy(xpath = "//*[contains(text(),'CheckedTextView')]")
+    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout" +
+            "/android.widget.FrameLayout/android.view.ViewGroup" +
+            "/android.widget.FrameLayout[2]/android.widget.LinearLayout" +
+            "/android.widget.ScrollView" +
+            "/android.widget.LinearLayout/android.widget.CheckedTextView")
+    WebElement CheckedTextView;
+
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
+    WebElement registerNewAccountBtn;
+
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_cancel_button")
+    WebElement registrationCancelBtn;
+
+    // New Page with budget
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/decor_content_parent")
+    WebElement viewGroup;
+
 
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements( new AppiumFieldDecorator(appiumDriver), this);
--- src/test/java/scenarios/nativeMobileTests.java
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -1,5 +1,7 @@
 package scenarios;
 
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import setup.BaseTest;
 
@@ -7,8 +9,26 @@ public class nativeMobileTests extends BaseTest {
 
     @Test(groups = {"native"}, description = "This simple test just click on the Sign In button")
     public void simpleNativeTest() throws IllegalAccessException, NoSuchFieldException, InstantiationException {
+        //getPo().getWelement("signInBtn").click();
+        //System.out.println("Simplest Android native test done");
+
+        //getPo().getWelement("loginEmailField").sendKeys("rafael-rezyapov@mail.ru");
+        //getPo().getWelement("login_pwdField").sendKeys("12345678qQ");
+        //getPo().getWelement("signInBtn").click();
+
+        //REgister New account
+        getPo().getWelement("registerBtn").click();
+        getPo().getWelement("eMailField").sendKeys("rafael-rezyapov@mail.ru");
+        getPo().getWelement("usernameField").sendKeys("Rafael");
+        getPo().getWelement("passwordField").sendKeys("12345678qQ");
+        getPo().getWelement("passwordConfirmField").sendKeys("12345678qQ");
+        getPo().getWelement("CheckedTextView").click();
+        getPo().getWelement("registerNewAccountBtn").click();
+
+        getPo().getWelement("loginEmailField").sendKeys("rafael-rezyapov@mail.ru");
+        getPo().getWelement("login_pwdField").sendKeys("12345678qQ");
         getPo().getWelement("signInBtn").click();
-        System.out.println("Simplest Android native test done");
+
 
     }
 
